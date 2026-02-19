26493baf5d78bf271fac3ba3b094866a8546ff8a
Stephen-Li-ICD2O1-01 <stephen.li@student.tdsb.on.ca>
2026-02-19 15:29:05 +0000

    update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,11 @@
+/*
+Lesson: Input
+Author: Stephen Li
+Date Created: February 17, 2026
+Date Last Modified: February 17, 2026
+*/
+import java.util.Scanner;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -9,23 +17,48 @@ public class PracticeProblem {
 	}
 
 	public static void q1() {
-		//Write question 1 code here
+		Scanner input = new Scanner(System.in);
+		System.out.print("What is your name: ");
+		String string = input.nextLine();
+		System.out.println(string);
 	}
 
 	public static void q2() {
-		//Write question 2 code here
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input a whole number: ");
+		int num = input.nextInt();
+		System.out.println(num * 2 + 2);
 	}
 
 	public static void q3() {
-		//Write question 3 code here
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input a radius: ");
+		double num = input.nextDouble();
+		System.out.println(num * 2 * 3.14);
+		System.out.println(num * num * 3.14);
 	}
 
 	public static void q4() {
-		//Write question 4 code here
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input a first name: ");
+		String first = input.nextLine();
+		System.out.print("Input a last name: ");
+		String last = input.nextLine();
+		System.out.print("Input an age: ");
+		int age = input.nextInt();
+		System.out.println(last + ", " + first + " - " + age);
 	}
 
 	public static void q5() {
-		//Write question 5 code here
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input a name: ");
+		String name = input.nextLine();
+		System.out.print("Input an age: ");
+		int age = input.nextInt();
+		int iq = age;
+		System.out.print(name);
+		System.out.println(age + 3);
+		System.out.println(iq);
 	}
 
 }
